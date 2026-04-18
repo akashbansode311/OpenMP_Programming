4bcf087160aa8385c84ea78063956c48a6378c3d
Akash Bansode <119726870+akashbansode311@users.noreply.github.com>
2026-04-18 20:34:08 +0530

    Create 09.For_Ordered.c

---
+++ 09.For_Ordered.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+    omp_set_num_threads(4);
+
+    printf("Parallel loop with ordered output:\n");
+
+    #pragma omp parallel for ordered
+    for(int i = 0; i < 4; i++)
+    {
+        #pragma omp ordered
+        {
+            // This part runs in sequence (0 - 4)
+            printf("Thread %d processes teration %d\n", omp_get_thread_num(), i);
+        }
+    }
+return 0;
+}
