b4f2bbda199c7a587a311486101fc92bdb44de85
Akash Bansode <119726870+akashbansode311@users.noreply.github.com>
2026-04-20 00:20:19 +0530

    Update 07.For_Lastprivate.c

--- 07.For_Lastprivate.c
+++ 07.For_Lastprivate.c
@@ -9,9 +9,9 @@ int main()
     omp_set_num_threads(N);
     #pragma omp parallel for lastprivate(c)
     for(int i = 0; i < N;i++){
-        printf("The thred %d value is %d\n",omp_get_thread_num(),c);
+        printf("The thread %d value is %d\n",omp_get_thread_num(),c);
         c = a + b;    //private
-        printf("The thred %d value is %d\n",omp_get_thread_num(),c);
+        printf("The thread %d value is %d\n",omp_get_thread_num(),c);
     }
 printf("End of paralle region");
 
