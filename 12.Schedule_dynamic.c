c76c8dfff4ad7bc1cd48dfd34b736cc823209e56
Akash Bansode <119726870+akashbansode311@users.noreply.github.com>
2026-04-20 00:41:02 +0530

    Update 12.Schedule_dynamic.c

--- 12.Schedule_dynamic.c
+++ 12.Schedule_dynamic.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <omp.h>
 
+int main() {
+    #pragma omp parallel for schedule(dynamic, 2)
+    for (int i = 0; i < 10; i++) {
+        printf("Dynamic: i=%d thread=%d\n", i, omp_get_thread_num());
+    }
+    return 0;
+}
