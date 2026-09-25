da7f9dd61ba93ab239d95a488cf1d9b78b6929ab
Akash Bansode <119726870+akashbansode311@users.noreply.github.com>
2026-09-25 12:49:21 +0530

    Update 12.Schedule_dynamic.c

--- 12.Schedule_dynamic.c
+++ 12.Schedule_dynamic.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 
-int main() {
-    #pragma omp parallel for schedule(dynamic, 2)
-    for (int i = 0; i < 10; i++) {
-        printf("Dynamic: i=%d thread=%d\n", i, omp_get_thread_num());
+int main(int argc, char *argv[])
+{
+    omp_set_num_threads(2);
+
+    printf("With no chunksize passed:\n");
+
+    #pragma omp parallel for schedule(dynamic)
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Thread %d processes iteration %d.\n",
+               omp_get_thread_num(), i);
     }
+
+    printf("\nWith a chunksize of 2:\n");
+
+    #pragma omp parallel for schedule(dynamic, 2)
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Thread %d processes iteration %d.\n",
+               omp_get_thread_num(), i);
+    }
+
     return 0;
 }
