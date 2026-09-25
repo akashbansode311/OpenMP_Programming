e7132751f3273d578472b7310a21f8697d5b23e4
Akash Bansode <119726870+akashbansode311@users.noreply.github.com>
2026-09-25 15:35:25 +0530

    Create 15.Schedule_auto.c

---
+++ 15.Schedule_auto.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+    omp_set_num_threads(4);
+
+    #pragma omp parallel for schedule(auto)
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Thread %d processes iteration %d.\n",
+               omp_get_thread_num(), i);
+    }
+
+    return 0;
+}
